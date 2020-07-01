799c9494d126072a53c8405e2962952600f58b93
Aika <aiganysha@gmail.com>
2020-07-01 13:56:18 -0500

    Controllers and Views created. Working

---
+++ HealthySnacks/Controllers/HealthySnacksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthySnacks.Controllers
+{
+    public class HealthySnacksController : Controller 
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+
+        public HealthySnacksController()
+        {
+
+        }
+    }
+}
